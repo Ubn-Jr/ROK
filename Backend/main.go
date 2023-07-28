f7d13d99ca410319aa28addf2117e2017095751d
gnsamine <gunesamine0@gmail.com>
2023-07-28 14:35:46 +0300

    main added

---
+++ Backend/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	avatar "ROK/requests"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/avatargender", avatar.Create)
+	app.Get("/addassetitem", avatar.Addassets)
+	app.Get("/setskincolour", avatar.Setskincolour)
+
+	log.Fatal(app.Listen(":8000"))
+}
